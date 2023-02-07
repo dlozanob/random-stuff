59705cf5f2b65c406bed0f594d685693d9b8d570
dlozanob <dlozanob@unal.edu.co>
2023-02-07 14:58:29 -0500

    ''

--- meetPages/js/script.js
+++ meetPages/js/script.js
@@ -5,12 +5,12 @@ var tmr;
 
 var shift = -93;
 var damp = -0.04;
-var period = 1/6;
-var factor = 30;
+var period = 1/12;
+var factor = 15;
 var time = 0;
 
 tiles.style.left = "1500px";
-tmr = setInterval(displace, 60);
+tmr = setInterval(displace, 40);
 
 function displace() {
     let translation = factor*Math.exp(damp*(time + shift))*Math.sin(period*Math.PI*(time + shift));
